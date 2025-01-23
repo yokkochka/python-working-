ca18cab3f2662d1b62098fcbc5f60dac08cedb36
Ulyana Yunusova <u.a.yunusova@edu.mtuci.ru>
2025-01-23 18:08:53 +0300

    now push 2

--- Shcool/test2.py
+++ Shcool/test2.py
@@ -1,7 +1,29 @@
-<<<<<<< HEAD
 
+animals = ['cat', 'dog', 'duck', 'bird', 'fox']
 
+# append() - функция допавления в конец списка
+animals.append('camel')
+print(animals)
 
-print(bool(""))
-=======
->>>>>>> 94c7c0b93ffde353c486dcacbf189c772544a908
+# insert() - функция добавления элемента на определенное место (индекс) в списке
+animals.insert(2, "pig")
+print(animals)
+
+# remove() - функция удадения по ЗНАЧЕНИЮ
+animals.remove("bird")
+print(animals)
+
+# del - ключевое слово, которое позволяет удалить элемент по его индексу
+del animals[-1]
+print(animals)
+
+# sort() - сортирует по возрастанию (reverse = True - сортировка по уюыванию)
+animals.sort()
+print(animals)
+
+animals.sort(reverse=True)
+print(animals)
+
+# clear() - отчистка
+animals.clear()
+print(animals)
